ec545a1cb85094cb450ff2c36fe0646f83c79efc
VCG\jmarzulli <jmarzulli@valorem.com>
2018-08-17 23:44:01 -0700

    Get the JSON from the HUD and pass it to be displayed.

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 // index.js
-const path = require('path');
-const https = require('https');
-const dateformat = require('dateformat');
-const express = require('express');
-const exphbs = require('express-handlebars');
+const path = require("path");
+const https = require("http");
+const dateformat = require("dateformat");
+const express = require("express");
+const exphbs = require("express-handlebars");
 const port = 3000;
 
 const app = express();
 
 var getConfigOptions = {
-    hostname: 'localhost:8080',
-    path: '/settings',
-    method: 'GET'
+  hostname: "localhost",
+  path: "/settings",
+  method: "GET",
+  port: 8080
 };
 
 app.engine(
@@ -25,14 +26,37 @@ app.engine(
 app.set("view engine", ".hbs");
 app.set("views", path.join(__dirname, "views"));
 
+var jsonResultText = "";
+
+function getHudConfig() {
+  return new Promise((resolve, reject) => {
+    try {
+      https
+        .request(getConfigOptions, function(restRes) {
+          console.log("STATUS: " + restRes.statusCode);
+          restRes.on("data", function(jsonResult) {
+            console.log("BODY: " + jsonResult);
+            resolve(JSON.parse(JSON.parse(jsonResult)));
+          });
+        })
+        .end();
+    } catch (err) {
+      resolve({ error: err });
+    }
+  });
+}
+
 app.get("/", (request, response) => {
-  response.render("home", {
-    time: dateformat(Date.now(), "dd-mm-yy hh:MM:ss TT"),
-    configJson: ""
-  })
+  getHudConfig().then(function(jsonConfig) {
+    console.log("Render");
+    response.render("home", {
+      time: dateformat(Date.now(), "dd-mm-yy hh:MM:ss TT"),
+      configJson: jsonConfig
+    }); // This code works.
+  });
 });
 
-app.use(express.static(__dirname + '/public')); 
+app.use(express.static(__dirname + "/public"));
 
 app.use(function(request, response) {
   response.status(404);
